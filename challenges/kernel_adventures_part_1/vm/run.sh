fdf8cf094f9cb5bd180696dcba8089cec1ad60ef
Evan Hellman <ehellman@iastate.edu>
2022-08-06 17:46:07 -0400

    Initial commit for kernel_adventures_part_1 challenge.

---
+++ challenges/kernel_adventures_part_1/vm/run.sh
@@ -0,0 +1,13 @@
+1#!/bin/bash
+
+qemu-system-x86_64 \
+    -m 128M \
+    -nographic \
+    -kernel ./bzImage \
+    -append 'console=ttyS0 loglevel=3 oops=panic panic=1 kaslr' \
+    -monitor /dev/null \
+    -initrd ./rootfs.cpio.gz  \
+    -cpu qemu64 \
+    -smp cores=2
+    -device e1000,netdev=net0
+    -netdev user,id=net0,hostfwd=tcp:5555-:5555
